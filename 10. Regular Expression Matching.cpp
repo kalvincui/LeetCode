1bb070afc205506b93a3132d3f0607a0816b92b2
kalvin <36674507+kalvincui@users.noreply.github.com>
2022-08-18 17:13:11 -0400

    Create 10. Regular Expression Matching.cpp

---
+++ 10. Regular Expression Matching.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.size() == 0){
+            return s.size()==0;
+        }
+        if (p.size()>1 && p[1] == '*'){
+            if (isMatch(s,p.substr(2))){
+                return true;
+            }
+            if ((p[0] == s[0] || p[0] == '.') && s.size()> 0){
+                return isMatch (s.substr(1), p);
+            }
+            return false;
+        }else {
+            if (p[0] == s[0] || p[0] == '.' && s.size()>0){
+                return isMatch(s.substr(1), p.substr(1));
+            }
+            return false;
+        }
+    }
+};
