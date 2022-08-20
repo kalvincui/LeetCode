26d6a546efd3ad5b4f75266e770bf976eca20704
kalvin <36674507+kalvincui@users.noreply.github.com>
2022-08-19 21:24:40 -0400

    Create 14. Longest Common Prefix.cpp

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string output = "";
+        int index = 0;
+        while (index < strs[0].size()){
+            char temp = strs[0][index];
+            for (int i = 0; i<strs.size(); i++){
+                if (index >= strs[i].size() || strs[i][index] !=temp){
+                    return output;
+                }
+            }
+            output += temp;
+            index++;
+        }
+        return output;
+    }
+};
