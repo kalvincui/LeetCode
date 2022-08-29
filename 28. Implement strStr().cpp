fc104372c84d1669e4cb8a0450609ff6e831395e
kalvin <36674507+kalvincui@users.noreply.github.com>
2022-08-29 13:20:39 -0400

    Update 28. Implement strStr().cpp

--- 28. Implement strStr().cpp
+++ 28. Implement strStr().cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        return haystack.find(needle);
+        if (needle.size() < 1){
+            return 0;
+        }
+        int index = 0;
+        while (needle.size() <= haystack.size()-index){
+            bool checker = true;
+            for (int j = 0; j<needle.size(); j++){
+                if (needle[j] == haystack[index+j]){
+                    continue;
+                }else {
+                    checker = false;
+                    break;
+                }
+            }
+            if (checker){
+                return index;
+            }
+            index++;
+        }
+        return -1;
     }
 };
