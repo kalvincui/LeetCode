25bee675f378b214436d83e4346f68a15b0d7657
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-06-08 14:39:43 -0400

    Update 1448. Count Good Nodes in Binary Tree.cpp

--- 1448. Count Good Nodes in Binary Tree.cpp
+++ 1448. Count Good Nodes in Binary Tree.cpp
@@ -34,3 +34,38 @@ public:
         return;
     }
 };
+
+//Alternative solution, just keeping track of max value rather than entire path
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    int goodNodes(TreeNode* root) {
+        //Record path to every node? 
+        //Find max element in path, if it is equal to current node, then it is a good node
+        //DFS
+        helper (root, root->val);
+        return ans;
+    }
+    void helper (TreeNode* node, int maxval){
+        if (node == NULL){
+            return;
+        }
+        if (maxval <= node->val){
+            ans++;
+            maxval = node->val;
+        }
+        helper (node->left, maxval);
+        helper (node->right, maxval);
+    }
+};
