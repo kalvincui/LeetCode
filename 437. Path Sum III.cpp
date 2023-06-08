60320033ddd8e4df917239fc30811a5bb45a413b
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-06-08 15:01:39 -0400

    Update 437. Path Sum III.cpp

--- 437. Path Sum III.cpp
+++ 437. Path Sum III.cpp
@@ -33,3 +33,19 @@ public:
         helper (node->right, targetSum, path);
     }
 };
+
+
+//Alternative Solution
+class Solution {
+public:
+    int pathSum(TreeNode* root, int sum) {
+        if(!root) return 0;
+        return sumUp(root, 0, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
+    }
+private:
+    int sumUp(TreeNode* root, int pre, int& sum){
+        if(!root) return 0;
+        int current = pre + root->val;
+        return (current == sum) + sumUp(root->left, current, sum) + sumUp(root->right, current, sum);
+    }
+};
