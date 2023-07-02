fd2b472bea9c590034de95e8ba6b11d0a07d7aa8
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-07-02 16:19:21 -0400

    Update 841. Keys and Rooms.cpp

--- 841. Keys and Rooms.cpp
+++ 841. Keys and Rooms.cpp
@@ -23,3 +23,26 @@ public:
         return true;
     }
 };
+
+//Recursive Solution
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector <bool> visited (rooms.size(), false);
+        visitRooms(rooms, visited, 0);
+        for (int i = 0; i<visited.size(); i++){
+            if (!visited[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    void visitRooms (vector<vector<int>>& rooms, vector<bool>& visited, int idx){
+        if (!visited[idx]){
+            visited[idx] = true;
+            for (int i = 0; i<rooms[idx].size(); i++){
+                visitRooms(rooms, visited, rooms[idx][i]);
+            }
+        }
+    }
+};
