287b3bb32a2123beb35e043c6264782927d5662f
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-07-04 13:15:59 -0400

    Update 104. Maximum Depth of Binary Tree.cpp

--- 104. Maximum Depth of Binary Tree.cpp
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -23,3 +23,42 @@ public:
         return max(left, right);
     }
 };
+
+
+//Iterative BFS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int longest = 0;
+        if (root){
+            queue <TreeNode*> q;
+            q.push(root);
+            while (!q.empty()){
+                int size = q.size();
+                for (int i = 0; i<size; i++){
+                    TreeNode* cur = q.front();
+                    q.pop();
+                    if (cur->left){
+                        q.push(cur->left);
+                    }
+                    if (cur->right){
+                        q.push(cur->right);
+                    }
+                }
+                longest++;
+            }
+        }
+        return longest;
+    }
+};
