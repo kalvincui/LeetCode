f225506fc9de200e996130c853bf431850682b57
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-07-04 11:41:37 -0400

    Update 7. Reverse Integer.cpp

--- 7. Reverse Integer.cpp
+++ 7. Reverse Integer.cpp
@@ -1,16 +1,17 @@
-class Solution {
 public:
     int reverse(int x) {
         int output = 0;
-        while (x){
-            int digit = x%10;
-            x /=10;
-            if (output > INT_MAX/10 || output < INT_MIN/10){
+        while (x!=0){
+            int digit = x % 10;
+            x/=10;
+            if (output > INT_MAX/10 || (output == INT_MAX/10 && digit > 7)){
                 return 0;
             }
-            output*=10;
+            if (output < INT_MIN/10 || (output == INT_MIN / 10 && digit < -8)){
+                return 0;
+            }
+            output *=10;
             output += digit;
         }
         return output;
     }
-};
