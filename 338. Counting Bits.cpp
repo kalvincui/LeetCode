e19060e621916b64629344ad57afc072ad15d51d
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-07-10 11:09:45 -0400

    Update 338. Counting Bits.cpp

--- 338. Counting Bits.cpp
+++ 338. Counting Bits.cpp
@@ -40,3 +40,31 @@ public:
         return ans;
     }
 };
+
+
+//Alternative Solution that doesn't require int32_t holder
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+		
+		// iterating fromt 0 to n
+        for(int i = 0; i<=n; i++)
+        {
+			// sum is initialised as 0
+            int sum = 0;
+            int num = i;
+			// while num not equals zero
+            while(num != 0)
+            {
+				// we have to count 1's in binary representation of i, therefore % 2
+                sum += num%2;
+                num = num/2;
+            }
+			// add sum to ans vector
+            ans.push_back(sum);
+        }
+		// return 
+        return ans;
+    }
+};
