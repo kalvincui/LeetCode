5e93fa72cc58d121aee5f7cd53844695c7524010
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-07-10 11:31:10 -0400

    Create 136. Single Number.cpp

---
+++ 136. Single Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if (nums.size() == 1){
+            return nums[0];
+        }
+        sort (nums.begin(), nums.end());
+        for (int i = 0; i<nums.size(); i+=2){
+            if (nums[i] != nums[i+1]){
+                return nums[i];
+            }
+        }
+        cout << nums[nums.size()-1];
+        return nums[nums.size()-1];
+    }
+};
