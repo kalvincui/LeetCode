5c6ce6cee01bc89aa658919a3823e445e6110bdf
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-07-10 11:06:23 -0400

    Update 338. Counting Bits.cpp

--- 338. Counting Bits.cpp
+++ 338. Counting Bits.cpp
@@ -18,3 +18,25 @@ public:
         return ans;
     }
 };
+
+
+
+//Alternative Solution that is Faster (simplifying while loop)
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector <int> ans;
+        int32_t holder = 0;
+        for (int i = 0; i<=n; i++){
+            int32_t temp = holder;
+            int ones = 0;
+            while (temp != 0){
+                temp = temp&(temp-1);
+                ones++;
+            }
+            ans.push_back(ones);
+            holder++;
+        }
+        return ans;
+    }
+};
