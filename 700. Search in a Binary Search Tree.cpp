0958b8e2252d98f3ef5c022c9694ef2deba35881
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-07-11 09:52:01 -0400

    Update 700. Search in a Binary Search Tree.cpp

--- 700. Search in a Binary Search Tree.cpp
+++ 700. Search in a Binary Search Tree.cpp
@@ -44,3 +44,18 @@ public:
         return root;
     }
 };
+
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (root){
+            if (root->val < val){
+                return searchBST(root->right, val);
+            }else if (root->val > val){
+                return searchBST(root->left, val);
+            }
+        }
+        return root;
+    }
+};
