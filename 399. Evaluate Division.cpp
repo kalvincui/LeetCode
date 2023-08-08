3ce53353d977e811581d88184b8da15e6bf3920b
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-08-08 10:04:35 -0400

    Update 399. Evaluate Division.cpp

--- 399. Evaluate Division.cpp
+++ 399. Evaluate Division.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
-    unordered_map<string, vector<pair<string, double>>> graph;
+    unordered_map <string, vector<pair<string, double>>> graph;
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        
-        int n = equations.size();
-        for (int i = 0; i < n; i++) {
-            string a = equations[i][0];
-            string b = equations[i][1];
-            double val = values[i];
-            
-            graph[a].push_back({b, val});
-            graph[b].push_back({a, (double)1/val});
+        //Create an adjacency list for every string
+        //In the list should contain the string it can go to + cost (pair)
+        //Since no zero division, you can have both forward and backward traversal
+        //Go through all the queries, performing DFS starting from the first trying to go to second
+        set <string> check;
+        vector <double> ans;
+        for (int i = 0; i<equations.size(); i++){
+            graph[equations[i][0]].push_back({equations[i][1], values[i]});
+            graph[equations[i][1]].push_back({equations[i][0], 1/values[i]});
+            check.insert(equations[i][0]);
+            check.insert(equations[i][1]);
         }
-        
-        vector<double> result;
-        for (auto query : queries) {
-            unordered_set<string> visited;
-            result.push_back(dfs(query[0], query[1], visited));
+        for (auto q : queries){
+            set <string> visited;
+            if (check.count(q[0]) == 0 || check.count(q[1]) == 0){
+                ans.push_back(-1);
+            }else {
+                visited.insert(q[0]);
+                ans.push_back(dfs(q[0], q[1], visited));
+            }
         }
-        return result;
+        return ans;
     }
-    
-    double dfs(string src, string dst, unordered_set<string> &visited) {
-        if (graph.find(src) == graph.end()) return -1;
-        if (src == dst) return 1;
-        
-        for (auto node : graph[src]) {
-            if (visited.count(node.first)) continue;
-            visited.insert(node.first);
-            double res = dfs(node.first, dst, visited);
-            if (res != -1) return res * node.second;
+    double dfs (string start, string target, set <string>& visited){
+        if (start == target){
+            return 1;
+        }
+        for (auto node : graph[start]){
+            if (visited.count(node.first) == 0){
+                visited.insert(node.first);
+                double res = dfs(node.first, target, visited);
+                if (res != -1){
+                    return res * node.second;
+                }
+            }
         }
         return -1;
     }
