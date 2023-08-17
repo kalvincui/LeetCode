255b57c72b955f78ee4bc708d2c537878aa717d3
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-08-17 14:36:38 -0400

    Update 216. Combination Sum III.cpp

--- 216. Combination Sum III.cpp
+++ 216. Combination Sum III.cpp
@@ -3,25 +3,20 @@ public:
     vector<vector<int>> ans;
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> numbers;
-        findCombination(k, n, numbers, 0);
+        findCombination(k, n, numbers, 1, 0);
         return ans;
     }
-    void findCombination(int k, int n, vector<int>numbers, int sum){
+    void findCombination(int k, int n, vector<int>numbers, int start, int sum){
         if (numbers.size() == k){
             if (sum == n){
-                sort(numbers.begin(), numbers.end());
-                if (find(ans.begin(), ans.end(), numbers) == ans.end()){
-                    ans.push_back(numbers);
-                }
+                ans.push_back(numbers);
             }
             return;
         }
-        for (int i = 1; i<=9; i++){
-            if (find(numbers.begin(), numbers.end(), i) == numbers.end()){
-                numbers.push_back(i);
-                findCombination(k, n, numbers, sum+i);
-                numbers.pop_back();
-            }
+        for (int i = start; i<=9; i++){
+            numbers.push_back(i);
+            findCombination(k, n, numbers, i+1, sum+i);
+            numbers.pop_back();
         }
         return;
     }
