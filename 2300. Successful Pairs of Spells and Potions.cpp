f8fc86be338d42fb53d9164ef43b4956c8681faa
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-08-17 11:41:26 -0400

    Update 2300. Successful Pairs of Spells and Potions.cpp

--- 2300. Successful Pairs of Spells and Potions.cpp
+++ 2300. Successful Pairs of Spells and Potions.cpp
@@ -10,30 +10,21 @@ public:
         //Another Idea:
         //Sort the potions 
         //Binary search using spells[i] and potions, returning index of min value that satisfies
-        //Push back potions.size() - index + 1;
+        //Push back potions.size() - low
         vector <int> pairs;
         sort (potions.begin(), potions.end());
         for (int i = 0; i<spells.size(); i++){
             int low = 0;
             int high = potions.size()-1;
-            int pos = -1;
             while (low <= high){
                 int mid = low + (high-low)/2;
-                if (spells[i] > int(ceil((double)success/potions[mid]))){
-                    high = mid-1;
-                    pos = mid;
-                }else if (spells[i] == int(ceil((double)success/potions[mid]))){
-                    pos = mid;
+                if (spells[i] >= int(ceil((double)success/potions[mid]))){
                     high = mid-1;
                 }else {
                     low = mid+1;
                 }
             }
-            if (pos == -1){
-                pairs.push_back(0);
-            }else {
-                pairs.push_back(potions.size()-pos);
-            }
+            pairs.push_back(potions.size()-low);
         }
         return pairs;
     }
