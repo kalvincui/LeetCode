66e2ba07c829358c9a9fb16a4d43f4e62cf721be
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-08-17 13:44:16 -0400

    Update 17. Letter Combinations of a Phone Number.cpp

--- 17. Letter Combinations of a Phone Number.cpp
+++ 17. Letter Combinations of a Phone Number.cpp
@@ -19,3 +19,37 @@ public:
         }
     }
 };
+
+
+
+
+class Solution {
+public:
+    unordered_map <char, string> map;   
+    vector<string> ans;
+    vector<string> letterCombinations(string digits) {
+        map['2'] = "abc";
+        map['3'] = "def";
+        map['4'] = "ghi";
+        map['5'] = "jkl";
+        map['6'] = "mno";
+        map['7'] = "pqrs";
+        map['8'] = "tuv";
+        map['9'] = "wxyz";
+        if (digits.size() == 0){
+            return ans;
+        }
+        getCombinations(digits, 0, "");
+        return ans;
+    }
+    void getCombinations (string digits, int idx, string s){
+        if (s.size() == digits.size()){
+            ans.push_back(s);
+            return;
+        }
+        for (int i = 0; i<map[digits[idx]].size(); i++){
+            getCombinations(digits, idx+1, s+map[digits[idx]][i]);
+        }
+        return;
+    }
+};
