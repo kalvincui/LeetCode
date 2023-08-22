264a271ee4b7d20994a83d1babd98c02d999ce71
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-08-22 10:21:29 -0400

    Create 611. Valid Triangle Number.cpp

---
+++ 611. Valid Triangle Number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int ans = 0;
+    int triangleNumber(vector<int>& nums) {
+        //Triangle is valid only if sum of any two sides is greater than third side for all sides
+        //Brute force check every possible triplet for this condition
+        //How do you get every possible triplet?
+        //Sort the nums, so it goes in ascending order
+        //Use recursion to get all possible triplets
+        //TLE but gives right answer
+        sort (nums.begin(), nums.end());
+        vector<int> triplet;
+        testTriplets(nums, triplet, 0);
+        return ans;
+    }
+    void testTriplets (vector<int>& nums, vector<int> t, int idx){
+        if (nums.size() <3){
+            return;
+        }
+        if (t.size() == 3){
+            if (t[0] + t[1] > t[2] && t[0] + t[2] > t[1] && t[1] + t[2] > t[0]){
+                ans++;
+            }
+            return;
+        }
+        for (int i = idx; i<nums.size(); i++){
+            t.push_back(nums[i]);
+            testTriplets(nums, t, i+1);
+            t.pop_back();
+        }
+    }
+};
