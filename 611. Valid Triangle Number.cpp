b3d5a7b7f778814781f66cec00c8223a1b41c781
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-08-22 10:40:07 -0400

    Update 611. Valid Triangle Number.cpp

--- 611. Valid Triangle Number.cpp
+++ 611. Valid Triangle Number.cpp
@@ -30,3 +30,25 @@ public:
         }
     }
 };
+
+
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int ans = 0;
+        sort (nums.begin(), nums.end());
+        for (int k = 2; k<nums.size(); k++){
+            int i = 0;
+            int j = k-1;
+            while (i<j){
+                if (nums[i] + nums[j] > nums[k]){
+                    ans += j-i;
+                    j--;
+                }else {
+                    i++;
+                }
+            }
+        }
+        return ans;
+    }
+};
