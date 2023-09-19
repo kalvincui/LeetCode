7d14ac4b9f95154f04a84ecc63a236691fc9ae11
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-09-18 22:24:36 -0400

    Update 287. Find the Duplicate Number.cpp

--- 287. Find the Duplicate Number.cpp
+++ 287. Find the Duplicate Number.cpp
@@ -1,3 +1,4 @@
+//Not constant extra space
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
@@ -8,6 +9,29 @@ public:
             if (originalsize==check.size()){
                 return nums[i];
             }
+        }
+        return -1;
+    }
+};
+
+
+//Constant Extra Space Solution
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        if (nums.size()>1){
+            int slow = nums[0];
+            int fast = nums[nums[0]];
+            while (slow!=fast){
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+            fast = 0;
+            while (fast!=slow){
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+            return slow;
         }
         return -1;
     }
