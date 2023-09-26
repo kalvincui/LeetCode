00d11f219aaf34b898f0e59ef18bc2aac871409c
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-09-26 01:06:02 -0400

    Update 316. Remove Duplicate Letters.cpp

--- 316. Remove Duplicate Letters.cpp
+++ 316. Remove Duplicate Letters.cpp
@@ -29,3 +29,36 @@ public:
         return ans;
     }
 };
+
+
+//Alternative solution less memory
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector <int> lastindex (26, 0);
+        unordered_map <char, bool> visited;
+        stack <char> st;
+        string ans = "";
+        for (int i = 0; i<s.size(); i++){
+            lastindex[s[i] - 'a'] = i;
+            visited[s[i]] = false;
+        }
+        for (int i = 0; i<s.size(); i++){
+            if (visited[s[i]]){
+                continue;
+            }
+            while (!st.empty() && st.top() > s[i] && lastindex[st.top() - 'a'] > i){
+                visited[st.top()] = false;
+                st.pop();
+            }
+            st.push(s[i]);
+            visited[s[i]] = true;
+        }
+        while (!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
