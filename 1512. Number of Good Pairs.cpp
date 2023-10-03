85705958412553e82c5d3b94c6b59c620a2738dd
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-10-03 19:38:32 -0400

    Update 1512. Number of Good Pairs.cpp

--- 1512. Number of Good Pairs.cpp
+++ 1512. Number of Good Pairs.cpp
@@ -12,3 +12,17 @@ public:
         return ans;
     }
 };
+
+//Alternative Solution
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map <int, int> map;
+        int ans = 0;
+        for (int i = 0; i<nums.size(); i++){
+            ans += map[nums[i]];
+            map[nums[i]]++;
+        }
+        return ans;
+    }
+};
