7adf6931688ac05735010ff9df9320554eaa0997
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-10-05 01:11:16 -0400

    Update 45. Jump Game II.cpp

--- 45. Jump Game II.cpp
+++ 45. Jump Game II.cpp
@@ -3,10 +3,7 @@ public:
     int jump(vector<int>& nums) {
         vector <int> dp (nums.size()+1, nums.size()-1);
         dp[0] = 0;
-        for (int j = 0; j<=nums[0] && j<nums.size(); j++){
-            dp[j] = min(dp[0]+1, j);
-        }
-        for (int i = 1; i<nums.size(); i++){
+        for (int i = 0; i<nums.size(); i++){
             for (int j = 0; j<=nums[i] && i+j<nums.size(); j++){
                 dp[i+j] = min (dp[i] + 1, dp[i+j]);
             }
