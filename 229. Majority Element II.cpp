10490b56970d13c67ffcdd04329824beb3f21093
kalvin <36674507+kalvincui@users.noreply.github.com>
2023-10-05 01:26:51 -0400

    Update 229. Majority Element II.cpp

--- 229. Majority Element II.cpp
+++ 229. Majority Element II.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         unordered_map<int,int> map;
-        set <int> ans;
         vector<int> res;
         for (int i = 0; i<nums.size(); i++){
             map[nums[i]]++;
-            if (map[nums[i]] > nums.size()/3){
-                ans.insert(nums[i]);
-            }
         }
-        for (auto itr = ans.begin(); itr!=ans.end(); itr++){
-            res.push_back(*itr);
+        for (auto itr = map.begin(); itr!=map.end(); itr++){
+            if (itr->second > nums.size()/3){
+                res.push_back(itr->first);
+            }
         }
         return res;
     }
